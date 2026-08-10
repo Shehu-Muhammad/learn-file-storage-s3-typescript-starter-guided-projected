2e731b15a07110367e5083a9557460e492c663b1
Shehu Muhammad <shehuaamuhammad@gmail.com>
2026-08-10 16:49:01 -0400

    completed handlerUploadThumbnail function

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,9 +1,9 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
+import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
 
 type Thumbnail = {
   data: ArrayBuffer;
@@ -48,6 +48,31 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   console.log("uploading thumbnail for video", videoId, "by user", userID);
 
   // TODO: implement the upload here
+  const formData = await req.formData();
+  const file = formData.get("thumbnail");
+  if (!(file instanceof File)) {
+    throw new BadRequestError("Thumbnail file missing");
+  }
 
-  return respondWithJSON(200, null);
+  const MAX_UPLOAD_SIZE = 10 << 20; // 10 MB
+  if (file.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError("Thumbnail file too large");
+  }
+
+  const mediaType = file.type;
+  const data = await file.arrayBuffer();
+  const videoMetadata = getVideo(cfg.db, videoId);
+  if (!videoMetadata) {
+    throw new NotFoundError("Couldn't find video");
+  }
+  if (videoMetadata.userID !== userID) {
+    throw new UserForbiddenError("You are not allowed to upload a thumbnail for this video");
+  }
+  videoThumbnails.set(videoId, { data, mediaType });
+
+  const thumbnailURL = `http://localhost:${cfg.port}/api/thumbnails/${videoId}`;
+  const video = {...videoMetadata, thumbnailURL};
+  updateVideo(cfg.db, video);
+
+  return respondWithJSON(200, video);
 }
